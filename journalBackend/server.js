1e661b04af05311b5142bcca9528a07d1eac0e1c
Srishti <srihti.pandey@wohlig.com>
2025-08-01 10:46:24 +0530

    added '/' route

--- journalBackend/server.js
+++ journalBackend/server.js
@@ -7,6 +7,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Add a root route handler
+app.get('/', (req, res) => {
+  res.json({ message: 'Journal API is running' });
+});
+
 const entryRoutes = require('./routes/entryRoutes');
 app.use('/api', entryRoutes);
 
@@ -15,7 +20,8 @@ mongoose.connect(process.env.MONGO_URI, {
   useUnifiedTopology: true
 }).then(() => {
   console.log('MongoDB connected');
-  app.listen(5000, () => console.log('Server running on port 5000'));
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }).catch((err) => {
   console.error('MongoDB connection error:', err);
 });
